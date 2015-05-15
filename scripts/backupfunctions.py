949da29182b7c4bda7a1e8540d2fe2358bbc56c5
Mayank Rungta <mr.mynk@gmail.com>
2015-05-15 21:09:58 +0530

    Deleting redundant file

--- scripts/backupfunctions.py
+++
@@ -1,9 +0,0 @@
-import os
-def savedb(db,filename,filepath):
-  DB_HOST = 'localhost'
-  DB_USER = 'root'
-  DB_USER_PASSWORD = 'ccmpProject**'
-  fullfilepath=filepath+filename
-  dumpcmd = "mysqldump -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " | gzip -c | cat > " + filepath + "/" + filename + ".sql.gz" 
-  print dumpcmd
-  os.system(dumpcmd)
