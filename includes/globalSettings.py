80f28a30a015113369b79f8c7fa83ca05233ad89
rajesh242 <togoli@gmail.com>
2015-06-07 10:52:44 +0530

    Added Global settins File

---
+++ includes/globalSettings.py
@@ -0,0 +1,3 @@
+broadcastsReportFile="/var/www/html/broadcasts/reports/broadcasts/index.html"
+broadcastReportFilePath="/var/www/html/broadcasts/reports/broadcasts/"
+includedir="/home/libtech/repo/includes/"
