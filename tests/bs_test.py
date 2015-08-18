e996c5779a01f5978cc1dbb9e35f1ba18667cf9d
Mayank Rungta <mr.mynk@gmail.com>
2015-08-18 21:28:48 +0530

    Clipping unecessary stuff

--- tests/bs_test.py
+++ tests/bs_test.py
@@ -2,22 +2,21 @@ import time
 
 from logger import loggerFetch
 from sn import driverInitialize,driverFinalize,displayInitialize,displayFinalize
-import requests
-from bs4 import BeautifulSoup, Tag
+
+from bs4 import BeautifulSoup
 
 #######################
 # Global Declarations
 #######################
 
-delay = 2
 timeout = 10
+
+url = "http://www.nrega.telangana.gov.in/Nregs/FrontServlet?requestType=SmartCardreport_engRH&actionVal=debitLoagReport&id=1457@DOP$APOL&type=01/04/2015&listType="
 
 
 #############
 # Functions
 #############
-
-
 
 
 def runTestSuite():
@@ -27,44 +26,51 @@ def runTestSuite():
   display = displayInitialize(1)
   driver = driverInitialize()
 
-  url = "http://www.nrega.telangana.gov.in/Nregs/FrontServlet?requestType=SmartCardreport_engRH&actionVal=debitLoagReport&id=1457@DOP$APOL&type=01/04/2015&listType="
 
   driver.get(url)
   logger.info("Fetching...[%s]" % url)
   
   html_source = driver.page_source
-  logger.debug("HTML Fetched [%s]" % html_source)
-
-  filename="/tmp/1.html"
-  with open(filename, 'w') as html_file:
-    logger.info("Writing Prematurely[%s]" % filename)
-    html_file.write(html_source.encode('utf-8'))
 
   driver.get(url)
-  logger.info("Fetching...[%s]" % url)
+  logger.info("Fetching...[%s] for the purpose of refresh" % url)
   
   html_source = driver.page_source
   logger.debug("HTML Fetched [%s]" % html_source)
 
-  filename="/tmp/2.html"
-  with open(filename, 'w') as html_file:
-    logger.info("Writing Prematurely[%s]" % filename)
-    html_file.write(html_source.encode('utf-8'))
+  bs = BeautifulSoup(html_source, "html.parser")
+  # tr_norm = bs.findAll('tr', attrs={'class':'normalRow'})
+  # tr_alt = bs.findAll('tr', attrs={'class':'alternateRow'})
+  # tr_list = tr_norm + tr_alt
+  tr_list = bs.findAll('tr', attrs={'class':['normalRow', 'alternateRow']})
+  logger.debug(str(tr_list))
 
-  bs = BeautifulSoup(html_source)
-  tr = bs.find('tr',attrs={'class':'normalRow'})
-  logger.info("tr[%s]" % tr)
+  for tr in tr_list:
+  #  if 'class' in str(tr): # Mynk - Need to check why this fails - NOT a dict unless Tag type
+  #    print tr['class'] 
+  #  if 'class' not in tr:
+  #    logger.info(str(tr))
+  #    continue   
+  #  if tr['class'] != 'normalRow' or tr['class'] != 'alternateRow':
+  #    logger.info(str(tr))
+  #    continue
+      
+    td = tr.find('td')
+    td = td.findNext('td')
+    panchayat = td.text.strip()
+    logger.info("Panchayat[%s]", panchayat)
 
-  td = tr.find('td')
-  td = td.findNext('td')
-  panchayat = td.text.strip()
-  logger.info("Panchayat[%s]", panchayat)
+    elem = driver.find_element_by_link_text(panchayat)
+    elem.click()
+    time.sleep(1)
+    
+    
+    filename="/tmp/%s.html" % panchayat
+    with open(filename, 'w') as html_file:
+      logger.info("Writing Prematurely[%s]" % filename)
+      html_file.write(driver.page_source.encode('utf-8'))
 
-  elem = driver.find_element_by_link_text(panchayat)
-  print(elem)
-  elem.click()
-  time.sleep(1)
-
+    driver.back()
 
   driverFinalize(driver)
   displayFinalize(display)
