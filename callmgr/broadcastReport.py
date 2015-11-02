8812866e2e7c23b8e2d0223e6014726877749653
rajesh241 <togoli@gmail.com>
2015-11-02 12:08:16 +0530

    Made Broadcast report as per new infrastructure

--- callmgr/broadcastReport.py
+++ callmgr/broadcastReport.py
@@ -33,11 +33,14 @@ def updateBroadcastTable(cur,bid):
   else:
     cost = cost/100
   total=success+expired+failMaxRetry+pending
+  successPercentage=0
+  if (total > 0):
+    successPercentage=math.trunc(success*100/total)
   print str(success)+"  "+str(failMaxRetry)+"  "+str(expired)+"  "+str(pending)+"  "+str(total)+"  "+str(cost)
   isComplete=0
   if(pending == 0):
    isComplete=1
-  query="update broadcasts set completed="+str(isComplete)+",success="+str(success)+",cost="+str(cost)+",fail="+str(failMaxRetry)+",expired="+str(expired)+",pending="+str(pending)+",total="+str(total)+" where bid="+str(bid) 
+  query="update broadcasts set successP='"+str(successPercentage)+"',completed="+str(isComplete)+",success="+str(success)+",cost="+str(cost)+",fail="+str(failMaxRetry)+",expired="+str(expired)+",pending="+str(pending)+",total="+str(total)+" where bid="+str(bid) 
   print query
   cur.execute(query)
 
@@ -50,56 +53,71 @@ def main():
   cur.execute(query)
   query="use libtech"
   cur.execute(query)
-  print "Printing Broadcast reports"
-  myhtml=gethtmlheader()
-  myhtml+="<h1>Summary of Broadcasts</h1>"
-  myhtml+="<table>"
-  tableArray=['Broadcast ID', 'Broadcast Name','Start Date','retry','Total','Pending','Success','Fail','Expired','Success %','Cost','Detail Report'] 
-  myhtml+=arrayToHTMLLine('th',tableArray)
-  print myhtml
-  query="select bid,completed from broadcasts where bid>1000 and error=0 order by bid DESC"
+  query="select bid,completed,name from broadcasts where bid>1000 and error=0 order by bid DESC "
   print query
   cur.execute(query)
   results = cur.fetchall()
   for row in results:
-    bid=row[0]
-    completed=row[1]
-    print "Current Bid is"+str(bid)
-    updateBroadcastTable(cur,bid)
-    minretry = ''
-    if (completed == 0):
-      query="select max(retry) from callQueue where bid="+str(bid)
-      cur.execute(query)
-      row1=cur.fetchone()
-      minretry=row1[0]
-    query="select name,DATE_FORMAT(startDate,'%d-%M-%Y'),total,pending,success,fail,expired,cost from broadcasts where bid="+str(bid)
-    cur.execute(query)
-    row1=cur.fetchone()
-    name=row1[0]
-    startDate=str(row1[1])
-    total=row1[2]
-    pending=str(row1[3])
-    success=row1[4]
-    fail=str(row1[5])
-    expired=str(row1[6])
-    cost=str(row1[7])
-    successPercentage=0
-    if (total > 0):
-      successPercentage=math.trunc(success*100/total)
-    reportLink='<a href="./'+str(bid)+'_'+name.strip()+'.csv">Download</a>'
-    tableArray=[bid,name,startDate,minretry,total,pending,success,fail,expired,successPercentage,cost,reportLink] 
-    myhtml+=arrayToHTMLLine('td',tableArray)
-    #write csv report
-    csvname=broadcastReportFilePath+str(bid)+"_"+name.strip()+".csv"
-    query="select phone,DATE_FORMAT(callStartTime,'%d-%M-%Y') callTime,status,attempts,duration from callSummary where bid="+str(bid)
+    bid=str(row[0])
+    completed=str(row[1])
+    name=row[2]
+    print bid+"  "+name+"  "+completed
     query="select a.district,a.block,a.panchayat,c.phone,DATE_FORMAT(c.callStartTime,'%d-%M-%Y') callTime,c.status,c.attempts,c.duration,f.feedback,c.sid from addressbook a,callSummary c left join callFeedback f on c.sid=f.sid where c.phone=a.phone and bid="+str(bid)
-    if(completed == 0):
+    if(completed == '0'):
+      csvname=broadcastReportFilePath+str(bid)+"_"+name.replace(' ',"")+".csv"
+      print csvname
       writecsv(cur,query,csvname)
-  myhtml+="</table>"
-  myhtml+=gethtmlfooter()
-  filename="./ui/html/broadcastReports.html"
-  f=open(broadcastsReportFile,"w")
-  f.write(myhtml.encode("UTF-8"))
+      updateBroadcastTable(cur,bid) 
+# print "Printing Broadcast reports"
+# myhtml=gethtmlheader()
+# myhtml+="<h1>Summary of Broadcasts</h1>"
+# myhtml+="<table>"
+# tableArray=['Broadcast ID', 'Broadcast Name','Start Date','retry','Total','Pending','Success','Fail','Expired','Success %','Cost','Detail Report'] 
+# myhtml+=arrayToHTMLLine('th',tableArray)
+# print myhtml
+# query="select bid,completed from broadcasts where bid>1000 and error=0 order by bid DESC"
+# print query
+# cur.execute(query)
+# results = cur.fetchall()
+# for row in results:
+#   bid=row[0]
+#   completed=row[1]
+#   print "Current Bid is"+str(bid)
+#   updateBroadcastTable(cur,bid)
+#   minretry = ''
+#   if (completed == 0):
+#     query="select max(retry) from callQueue where bid="+str(bid)
+#     cur.execute(query)
+#     row1=cur.fetchone()
+#     minretry=row1[0]
+#   query="select name,DATE_FORMAT(startDate,'%d-%M-%Y'),total,pending,success,fail,expired,cost from broadcasts where bid="+str(bid)
+#   cur.execute(query)
+#   row1=cur.fetchone()
+#   name=row1[0]
+#   startDate=str(row1[1])
+#   total=row1[2]
+#   pending=str(row1[3])
+#   success=row1[4]
+#   fail=str(row1[5])
+#   expired=str(row1[6])
+#   cost=str(row1[7])
+#   successPercentage=0
+#   if (total > 0):
+#     successPercentage=math.trunc(success*100/total)
+#   reportLink='<a href="./'+str(bid)+'_'+name.strip()+'.csv">Download</a>'
+#   tableArray=[bid,name,startDate,minretry,total,pending,success,fail,expired,successPercentage,cost,reportLink] 
+#   myhtml+=arrayToHTMLLine('td',tableArray)
+#   #write csv report
+#   csvname=broadcastReportFilePath+str(bid)+"_"+name.strip()+".csv"
+#   query="select phone,DATE_FORMAT(callStartTime,'%d-%M-%Y') callTime,status,attempts,duration from callSummary where bid="+str(bid)
+#   query="select a.district,a.block,a.panchayat,c.phone,DATE_FORMAT(c.callStartTime,'%d-%M-%Y') callTime,c.status,c.attempts,c.duration,f.feedback,c.sid from addressbook a,callSummary c left join callFeedback f on c.sid=f.sid where c.phone=a.phone and bid="+str(bid)
+#   if(completed == 0):
+#     writecsv(cur,query,csvname)
+# myhtml+="</table>"
+# myhtml+=gethtmlfooter()
+# filename="./ui/html/broadcastReports.html"
+# f=open(broadcastsReportFile,"w")
+# f.write(myhtml.encode("UTF-8"))
 
 
 if __name__ == '__main__':
