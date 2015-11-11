e513ac99699e2a3dec7e8d49ff9a5ec37355234c
rajesh241 <togoli@gmail.com>
2015-11-11 12:30:39 +0530

    Modified exotel wage Broadcast script

--- misc/php/exotelWageBroadcast.php
+++ misc/php/exotelWageBroadcast.php
@@ -3,6 +3,9 @@ header('Content-type: text/plain');
 include ("./params.php");
 $mydbcon = mysqli_connect($dbserver,$dbuser,$dbpasswd);
 $callid=$_GET["CustomField"];
+#$callid='434146';
+#$callid='432171';
+#$callid='437082';
 file_put_contents("/tmp/myget.txt", $callid);
 $query="use libtech";
 mysqli_query($mydbcon,$query);
@@ -14,9 +17,14 @@ $audio=$row['audio'];
 #$audio="chattisgarh_wage_broadcast_static0,ashkala,chattisgarh_wage_broadcast_static1,200,2,chattisgarh_wage_broadcast_static2,300,18,chattisgarh_wage_broadcast_static3,14,august,2015,chattisgarh_wage_broadcast_static4,chattisgarh_wage_broadcast_repeat,chattisgarh_wage_broadcast_static0,ashkala,chattisgarh_wage_broadcast_static1,200,2,chattisgarh_wage_broadcast_static2,300,18,chattisgarh_wage_broadcast_static3,14,august,2015,chattisgarh_wage_broadcast_static4,chattisgarh_wage_broadcast_thankyou";
 #$audio="chattisgarh_wage_broadcast_static0,ashkala,chattisgarh_wage_broadcast_static1,85,chattisgarh_wage_broadcast_static2,300,18,chattisgarh_wage_broadcast_static3,14,august,2015,chattisgarh_wage_broadcast_static4,chattisgarh_wage_broadcast_repeat,chattisgarh_wage_broadcast_static0,ashkala,chattisgarh_wage_broadcast_static1,85,chattisgarh_wage_broadcast_static2,300,18,chattisgarh_wage_broadcast_static3,14,august,2015,chattisgarh_wage_broadcast_static4,chattisgarh_wage_broadcast_thankyou";
 #$audio="chattisgarh_wage_broadcast_static0,lundra,chattisgarh_wage_broadcast_static1";
+if(preg_match('/chattisgarh/',$audio)){
+  $prefix='hindi';
+}else{
+  $prefix='telugu1';
+}
 $audioarray=explode(",",$audio);
 foreach($audioarray as $singleaudio){
-$myaudio="http://libtech.info/audio/hindi/hindi_".$singleaudio.".wav\n";
+$myaudio="http://libtech.info/audio/".$prefix."/".$prefix."_".$singleaudio.".wav\n";
 echo $myaudio;
 }
 #file_put_contents("/tmp/myget.txt", $query);
