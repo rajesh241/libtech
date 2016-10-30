1063d9ed7dfc5cd51effbbb52fbed4a1045cee40
Mayank Rungta <mr.mynk@gmail.com>
2016-10-30 20:45:19 +0530

    Cleaning up and making python 3.x compliant

--- callmgr/awaazde.py
+++ callmgr/awaazde.py
@@ -7,7 +7,16 @@
 #
 #===============================================================================
 
+import os
+dirname = os.path.dirname(os.path.realpath(__file__))
+rootdir = os.path.dirname(dirname)
+
+import sys
+sys.path.insert(0, rootdir)
+
 import time
+
+from wrappers.logger import loggerFetch
 
 from xactclient.data.datamgr import TemplateMgr
 from xactclient.data.datamgr import CallMgr
@@ -20,7 +29,7 @@ WS_URL = 'https://awaaz.de/console/xact'
 vid=1
 
 
-def awaazdeUpload(file):
+def awaazdeUpload(logger, file):
     authdata= AuthData(USERNAME,PASSWORD,WS_URL)    
     templateMgr = TemplateMgr(authdata)
     callMgr = CallMgr(authdata);
@@ -32,22 +41,23 @@ def awaazdeUpload(file):
                 
     template = templateMgr.getTemplate(template_id)
 
-    # print("Template [%s]" % template)
+    # logger.info("Template [%s]" % template)
     template_data = {'text': template['text'], 'vocabulary': template['vocabulary'], 'language': template['language']}
-    print("Vocabulary %s" % template['vocabulary'])
+    #logger.info("Vocabulary %s" % template['vocabulary'])
 
     filename = file.strip('.wav')  # Strip any other kind of format it might have
+    logger.info('FileName[%s]' % filename)
     if filename not in template_data['vocabulary']:
-        print templateMgr.upload_file(template_id, file_url, is_url=True)
+        logger.info(templateMgr.upload_file(template_id, file_url, is_url=True))
         template_data['vocabulary'].append(filename)
-        print template_data
-        print templateMgr.update(template_id, template_data)
+        #logger.info(template_data)
+        logger.info(templateMgr.update(template_id, template_data))
     else:
-        print("File already present [%s]" % file)
+        logger.info("File already present [%s]" % file)
 
     return template
 
-def awaazdeStatusCheck(cur_callid=None):
+def awaazdeStatusCheck(logger, cur_callid=None):
     authdata= AuthData(USERNAME,PASSWORD,WS_URL)    
     templateMgr = TemplateMgr(authdata)
     callMgr = CallMgr(authdata);
@@ -58,57 +68,38 @@ def awaazdeStatusCheck(cur_callid=None):
     else:
         vendorcallid=cur_callid
 
-    print("Printing details for Call[%s]" % vendorcallid)
+    logger.info("CallDetails[%s]" % vendorcallid)
     call_details = callMgr.getCall(str(vendorcallid))
 
-    print call_details
+    logger.info('Call Details[%s]' % call_details)
     
     status = call_details['status']
     duration = status['duration']
-    print duration
-
     attempts = status['attempts']
-    print attempts
-    
     sent_on = status['sent_on']
-    print sent_on
-    
-    print status['response']
-
-    # url = call_details['url']
-    # print "DeliveryStatus[%s]" % url
-
     text = call_details['text']
-    print text
-    
-    print call_details['backup_calls']
-    print call_details['send_on']
-    
     recipient = call_details['recipient']
-    print recipient
-    
     callsid = call_details['id']
-    print callsid
     
     return duration, attempts, sent_on, text, recipient, callsid #, url
     
 
-def awaazdePlaceCall(phone, wave_file=None, calltime=None):
+def awaazdePlaceCall(logger, phone, wave_file=None, calltime=None):
     authdata= AuthData(USERNAME,PASSWORD,WS_URL)    
     templateMgr = TemplateMgr(authdata)
     callMgr = CallMgr(authdata);
 
     #Logic to upload the wave_file for the first time -
     if not wave_file:
-        wave_file = '1308_Rayapuramcampaignbhaskar.wav' # '1431_CgbcstChakeri010316.wav'    # This should be done only once for each new file
-    print("WaveFile[%s]" % wave_file)
+        wave_file = '1629_RSCD281016.wav' # '1308_Rayapuramcampaignbhaskar.wav' #     # This should be done only once for each new file
+    logger.info("WaveFile[%s]" % wave_file)
     filename = wave_file.strip('.wav')
     
     data  = {'recipient': phone, 'text':filename, 'backup_calls':0}  # , 'send_on': calltime}
 #    calltime=datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%S")#Schedule the call for now
 
     calldata=callMgr.create(data)
-    print calldata
+    logger.info("CallData[%s]" % calldata)
     vendorcallid=calldata['id']
 
     return vendorcallid
@@ -116,25 +107,29 @@ def awaazdePlaceCall(phone, wave_file=None, calltime=None):
 
 def main():
 
+    logger = loggerFetch("info")
+    logger.info("BEGIN PROCESSING...")
+
     phone='9845155447'
 
     #Logic to upload the wave_file for the first time -
-    wave_file = '1459_Venky18MAR2016MittadodiPanchayat.wav' # '1431_CgbcstChakeri010316.wav'    # This should be done only once for each new file
+    wave_file = '1629_RSCD281016.wav' # '1431_CgbcstChakeri010316.wav'    # This should be done only once for each new file
     filename = wave_file.strip('.wav')
 
     if wave_file:
-        print awaazdeUpload(wave_file)
+        logger.info(awaazdeUpload(logger, wave_file))
 
     if False:
-        vendorcallid = awaazdePlaceCall(phone, wave_file)
+        vendorcallid = awaazdePlaceCall(logger, phone, wave_file)
         time.sleep(10)
     else:
-        vendorcallid = '71489' # '70239' # '70221' # '57897'
-    print("Call ID [%s]" % vendorcallid)
+        vendorcallid = '71503' # '70239' # '70221' # '57897'
+    logger.info("Call ID [%s]" % vendorcallid)
 
-#    callreturn = awaazdeStatusCheck('70217')
-    callreturn = awaazdeStatusCheck(vendorcallid)
-    print callreturn
+    callreturn = awaazdeStatusCheck(logger, vendorcallid)
+    logger.info(callreturn)
+
+    logger.info("...END PROCESSING")     
 
  
 if __name__ =='__main__':main()
