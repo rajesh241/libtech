0ef07ab3da261dc82d586bf06d7fa900d638fa33
Mayank Rungta <mr.mynk@gmail.com>
2016-12-01 15:24:47 +0530

    Commenting the gat/suvery_no fetch lines

--- tests/saat-baara.py
+++ tests/saat-baara.py
@@ -46,15 +46,16 @@ def runTestSuite():
   logger.info("BEGIN PROCESSING...")
 
   
-
+  """
   for gat in range(1,200):
-    cmd = '''curl 'https://mahabhulekh.maharashtra.gov.in/Konkan/Home.aspx/getSnos' -X POST -H 'Accept: application/json, text/plain, */*' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.5' -H 'Connection: keep-alive' -H 'Content-Length: 60' -H 'Content-Type: application/json;charset=utf-8' -H 'Cookie: ASP.NET_SessionId=xgahrcwef0hoicteddbwrxxo' -H 'Host: mahabhulekh.maharashtra.gov.in' -H 'Referer: https://mahabhulekh.maharashtra.gov.in/Konkan/Home.aspx' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0' -d "{'ptxt':'%s','vid':'273200030399810000','did':'32','tid':'3'}" -o ./json/%s.json ''' % (gat, gat)
+    cmd = '''curl 'https://mahabhulekh.maharashtra.gov.in/Konkan/Home.aspx/getSnos' -X POST -H 'Accept: application/json, text/plain, */*' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.5' -H 'Connection: keep-alive' -H 'Content-Length: 60' -H 'Content-Type: application/json;charset=utf-8' -H 'Cookie: ASP.NET_SessionId=xgahrcwef0hoicteddbwrxxo' -H 'Host: mahabhulekh.maharashtra.gov.in' -H 'Referer: https://mahabhulekh.maharashtra.gov.in/Konkan/Home.aspx' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0' -d "{'ptxt':'%s','vid':'273200030399810000','did':'32','tid':'3'}" ''' # -o ./json/%s.json ''' % (gat, gat)
     logger.info('Executing [%s]' % cmd)
-    logger.info(os.system(cmd))
-    # break
+    json = os.system(cmd)
+    print(json)
+    break
 
   return
-
+  """
 
   display = displayInitialize(0)
   driver = driverInitialize()
