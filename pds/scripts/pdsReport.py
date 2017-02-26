9f2ab75a6b6c37970ee0a96e874cd33dcd42f4f4
rajesh241 <togoli@gmail.com>
2017-02-26 07:31:01 +0530

    Made changes for the bihar PDS Broadcast

--- pds/scripts/pdsReport.py
+++ pds/scripts/pdsReport.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import datetime
 import requests
 import MySQLdb
 import os
@@ -32,6 +33,7 @@ def argsFetch():
   parser.add_argument('-v', '--visible', help='Make the browser visible', required=False, action='store_const', const=1)
   parser.add_argument('-wr', '--webReport', help='Generate Web Report', required=False, action='store_const', const=1)
   parser.add_argument('-cf', '--checkAudioFiles', help='Check if AudioFiles are present', required=False, action='store_const', const=1)
+  parser.add_argument('-cb', '--createBroadcast', help='Create Broadcast', required=False, action='store_const', const=1)
   parser.add_argument('-f', '--fpsCode', help='FPS shop for which data needs to be donwloaded', required=False)
 
   args = vars(parser.parse_args())
@@ -86,6 +88,92 @@ def genWebReport(logger):
   writecsv(cur,query,csvname)
 
   dbFinalize(db) # Make sure you put this if there are other exit paths or errors
+
+def getPDSAudioList(fpsCode,month):
+  db = dbInitialize(db=pdsDB,host=pdsDBHost, charset="utf8")  # The rest is updated automatically in the function
+  cur=db.cursor()
+  db.autocommit(True)
+  query="select districtName from fpsShops where fpsCode='%s' " % fpsCode
+  cur.execute(query)
+  districtName=''
+  error=0
+  if cur.rowcount == 1:
+    row=cur.fetchone()
+    districtName=row[0]
+  else:
+    error=1
+  dbFinalize(db) # Make sure you put this if there are other exit paths or errors
+  db = dbInitialize(db='libtech', charset="utf8")  # The rest is updated automatically in the function
+  cur=db.cursor()
+  db.autocommit(True)
+  #Query to set up Database to read Hindi Characters
+  query="SET NAMES utf8"
+  cur.execute(query)
+  if districtName=='Gaya':
+    audioLanguage='gaya'
+  elif districtName=='Munger':
+    audioLanguage='maithili'
+  elif districtName=='Saharsa':
+    audioLanguage='maithili'
+  elif districtName=='Sitamarhi':
+    audioLanguage='sitamarhi'
+  else:
+    audioLanguage='maithili'
+    error=1
+
+  if month < 10:
+    monthString='bihar0'+str(month)
+  elif month > 12:
+    error=1
+    monthString='bihar'+str(month)
+  else:
+    monthString='bihar'+str(month)
+  audioList=[audioLanguage+'_0',monthString,audioLanguage+'_1',str(fpsCode),audioLanguage+'_2',monthString,audioLanguage+'_3',str(fpsCode),audioLanguage+'_4']
+  likeString='%biharPDS%'
+  audioIDs=''
+  for audioFile in audioList:
+    query="select id from audioLibrary where name='%s' and filename like '%s' " % (audioFile,likeString)
+    cur.execute(query)
+    if cur.rowcount > 0:
+      row=cur.fetchone()
+      audioIDs+=str(row[0])
+      audioIDs+=','
+    else:
+      error=1
+  audioIDs=audioIDs.rstrip(',')
+  dbFinalize(db) # Make sure you put this if there are other exit paths or errors
+  if error == 1:
+    return None
+  else:
+    return audioIDs
+
+def createPDSBroadcast(logger):
+  logger.info("Creating PDS Broadcast")
+  db = dbInitialize(host=pdsDBHost,db=pdsDB, charset="utf8")  # The rest is updated automatically in the function
+  cur=db.cursor()
+  db.autocommit(True)
+  #Query to set up Database to read Hindi Characters
+  query="SET NAMES utf8"
+  cur.execute(query)
+  monthLabels=['0','JAN','FEB','MAR','APR','MAY','JUNE','JULY','AUG','SEP','OCT','NOV','DEC']
+  query="select fs.id,f.districtName,f.blockName,f.village,fs.fpsCode,fs.month,fs.year from fpsShops f, fpsStatus fs where f.fpsCode=fs.fpsCode and fs.initiateBroadcast=1"
+  cur.execute(query)
+  results=cur.fetchall()
+  for row in results:
+    [rowid,districtName,blockName,village,fpsCode,month,year] = row
+    monthName=monthLabels[month]
+    broadcastName="%s_%s_%s_%s_%s_%s" % (str(rowid),districtName,blockName,village,monthName,year)
+    logger.info("Broadcast Name: %s " % broadcastName)
+    broadcastType='biharPDS'
+    vendor='any'
+    todayPlus3=datetime.date.fromordinal(datetime.date.today().toordinal()+3)
+    endDate=todayPlus3.strftime("%Y-%m-%d")
+    audioFiles=getPDSAudioList(fpsCode,month)
+    region='adri'
+    template='general'
+    query="insert into broadcasts (name,type,vendor,startDate,endDate,fileid,region,template) values ('%s','%s','%s',NOW(),'%s','%s','%s','%s')" % (broadcastName,broadcastType,vendor,endDate,audioFiles,region,template) 
+    logger.info(query)
+
 def main():
   args = argsFetch()
   logger = loggerFetch(args.get('log_level'))
@@ -96,6 +184,8 @@ def main():
     genWebReport(logger)
   if args['checkAudioFiles']:
     checkAudioFiles(logger)
+  if args['createBroadcast']:
+    createPDSBroadcast(logger)
 
   logger.info("...END PROCESSING")     
   exit(0)
