0bbd11db9e2418b2de84b69b97e35ac6e8e7469e
Mayank Rungta <mr.mynk@gmail.com>
2017-03-24 20:54:37 +0530

    Added a remark field to be updated. Need to add create complaint checkbox now

--- libtech/src/models/panchayats.ts
+++ libtech/src/models/panchayats.ts
@@ -1,5 +1,3 @@
-// User model based on the structure of github api at
-// https://api.github.com/users/{username}
 export interface Panchayat {
     panchayatName: string[];
 }
--- libtech/src/pages/transaction/transaction.ts
+++ libtech/src/pages/transaction/transaction.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     selector: 'page-transaction',
@@ -9,11 +10,25 @@ export class TransactionPage {
     jobcard: string;
     date: string;
     transaction: string;
+    url: string;
+    remarks: string;
+    field: FirebaseListObservable<any>;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private af: AngularFire) {
         this.jobcard = this.navParams.get('jobcard');
         this.date = this.navParams.get('date');
         this.transaction = this.navParams.get('transaction');
+        this.url = this.navParams.get('url') + '/' + this.date;
+        console.log(this.url);
+        this.field = this.af.database.list(this.url);
+        console.log(this.field);
+    }
+
+    setRemarkValue(remarkInput) {
+        console.log(remarkInput.value + '==' + this.remarks); // Can skip this.remarks Mynk
+        this.field.update("1", { remarks: remarkInput.value });
+        //        this.field.update("1", { remarks: remarkInput.value, createComplaint: complaintBox.value });
     }
 
     ionViewDidLoad() {
