d0741ae0dec124c4af918d477e8cb07b4f889b63
Mayank Rungta <mr.mynk@gmail.com>
2017-03-28 13:10:05 +0530

    Adding the functionality to fetch the Muster details for given Jobcard

--- nrega/crawl/downloadMustersAP.py
+++ nrega/crawl/downloadMustersAP.py
@@ -1,13 +1,11 @@
 from bs4 import BeautifulSoup
 import multiprocessing, time
 import requests
-import MySQLdb
 import os
 import os.path
 import time
 import re
 import sys
-from MySQLdb import OperationalError
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -38,17 +36,70 @@ def argsFetch():
 
   args = vars(parser.parse_args())
   return args
+
+def downloadJobcardHTML(logger, driver, jobcard):
+  '''
+  Download the Jobcard HTML with payment info (Muster Details)
+  '''
+  
+  jobcard_url = 'http://www.nrega.telangana.gov.in/Nregs/FrontServlet?requestType=HouseholdInf_engRH&actionVal=SearchJOB&JOB_No='
+  url = jobcard_url + jobcard
+
+  logger.info("Fetching...[%s]" % url)
+  driver.get(url)
+  logger.info("Fetched [%s]" % driver.title)
+
+  if driver.title != ':: Job Card Holders Information ::':
+    logger.info("Refreshing...[%s]" % url)
+    driver.get(url)    # A double refresh required for the page to load
+    logger.info("Refreshed [%s]" % driver.title) 
+
+  html_source = driver.page_source.replace('<head>',
+                                         '<head><meta http-equiv="Content-Type" '
+                                          'content="text/html; charset=utf-8"/>').encode('utf-8')
+  logger.debug("HTML Fetched [%s]" % html_source)
+
+  bs = BeautifulSoup(html_source, "html.parser")
+  span = bs.find('span',attrs={'class':'rpt-hd-txt'})
+  if not span:
+    logger.error("Span not found for jobcard[%s]" % (jobcard))
+    return None
+
+  """
+  query = None
+  ''' Update Only if there is a change for Google Drive will sync '''
+  if os.path.exists(filename):
+    with open(filename, 'r+') as html_file:
+      cur_source = html_file.read()
+      bs2 = BeautifulSoup(cur_source, "html.parser")
+      
+      if (bs.find(id='main3') != bs2.find(id='main3')):
+        html_file.seek(0)
+        html_file.write(html_source.decode('utf-8'))
+        html_file.truncate()
+        logger.info("Updating [%s]" % filename)
+      else:
+        logger.info("Skipping as no update")
+        query = 'update jobcardRegister set downloadDate="%s", isDownloaded=1 where jobcard="%s"' % (strftime('%Y-%m-%d %H:%M:%S'), jobcard)
+  else:
+    with open(filename, 'w') as html_file:
+      logger.info("Writing [%s]" % filename)
+      html_file.write(html_source.decode('utf-8'))
+
+  if not query:
+    query = 'update jobcardRegister set downloadDate="%s", isDownloaded=1, isProcessed=0 where jobcard="%s"' % (strftime('%Y-%m-%d %H:%M:%S'), jobcard)
+  logger.info('Executing query: [%s]', query)
+  cur = db.cursor()
+  cur.execute(query)
+  """
+  return html_source
+
+
 def main():
   args = argsFetch()
   logger = loggerFetch(args.get('log_level'))
   logger.info('args: %s', str(args))
   logger.info("BEGIN PROCESSING...")
-  db = dbInitialize(db=nregaDB, charset="utf8")  # The rest is updated automatically in the function
-  cur=db.cursor()
-  db.autocommit(True)
-  #Query to set up Database to read Hindi Characters
-  query="SET NAMES utf8"
-  cur.execute(query)
   display = displayInitialize(args['visible'])
   driver = driverInitialize(args['browser'])
   
@@ -57,11 +108,13 @@ def main():
   else:
     jobcard='141975701001010679'
 
-  logger.info("Jobcard: %s " % jobcard)
+  logger.info("Fetching Jobcard[%s]..." % jobcard)
+
+  html = downloadJobcardHTML(logger, driver, jobcard)
+  logger.info(html)
 
   driverFinalize(driver)
   displayFinalize(display)
-  dbFinalize(db) # Make sure you put this if there are other exit paths or errors
   logger.info("...END PROCESSING")     
   exit(0)
 if __name__ == '__main__':
