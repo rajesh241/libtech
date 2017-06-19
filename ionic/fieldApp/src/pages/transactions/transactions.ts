d9c315328f8ca4c6d2c852f475b1f38f50dfb72a
Mayank Rungta <mr.mynk@gmail.com>
2017-06-19 13:50:18 +0530

    Adding Vivek's changes for the restructuring of the Firebase schema

--- ionic/fieldApp/src/pages/transactions/transactions.ts
+++ ionic/fieldApp/src/pages/transactions/transactions.ts
@@ -14,14 +14,26 @@ export class TransactionsPage {
     jobcard: string;
     url: string;
     items: AfoListObservable<any[]>;
+    rejInv: AfoListObservable<any[]>;
+    applicantDetails: AfoListObservable<any[]>;
+    //Useful to toggle buttons.  Default is off.
+    show: boolean = false;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private afoDatabase: AngularFireOfflineDatabase) {
         this.panchayat = this.navParams.get('panchayatName');
         this.jobcard = this.navParams.get('jobcardNumber');
+        var ptCode = this.jobcard.substring(0, 13);
+        var vilCode = this.jobcard.substring(14, 17);
+        var h: string[] = this.jobcard.split('_');
+        var hhdCode = h[h.length - 1]
+        var geoUrl = '/geo/' + ptCode + '/' + vilCode + '/' + hhdCode + '/'
+        this.rejInv = afoDatabase.list(geoUrl);
+
+        var appUrl = geoUrl + 'applicants/'
+        this.applicantDetails = afoDatabase.list(appUrl);
+
         this.url = '/transactions/' + this.jobcard
         this.items = afoDatabase.list(this.url);
-        console.log(this.url);
-        console.log(this.items);
     }
 
     ionViewDidLoad() {
