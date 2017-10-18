8cbc593595f37b67dcae595143d68c1956be68a1
Mayank Rungta <mr.mynk@gmail.com>
2017-10-18 14:01:49 +0530

    Fixed the problem with unicode by changing response.text to response.content

--- src/tests/mnregaweb_panchregpeople.py
+++ src/tests/mnregaweb_panchregpeople.py
@@ -9,17 +9,20 @@ from wrappers.logger import loggerFetch
 import unittest
 import requests
 
-filename = 'requests.html' 
+filename = 'panchregpeople.html' 
 
+# Obtain the Registration Application Register page
+def fetch_regapp(logger):
+    # Obtain a cookie
+    url = 'http://mnregaweb2.nic.in/netnrega/IndexFrame.aspx?lflag=eng&District_Code=2721&district_name=AJMER&state_name=RAJASTHAN&state_Code=27&block_name=%E0%A4%85%E0%A4%B0%E0%A4%BE%E0%A4%88&block_code=2721001&fin_year=2017-2018&check=1&Panchayat_name=%E0%A4%B2%E0%A4%BE%E0%A4%AE%E0%A5%8D%E0%A4%AC%E0%A4%BE&Panchayat_Code=2721001029'
+    response = requests.get(url)
+    logger.debug(response.text)
+    cookies = response.cookies
+    logger.info(cookies)
 
-def fetch_panchayat(logger):
-    url = 'http://mnregaweb2.nic.in/netnrega/Citizen_html/Panchregpeople.aspx'
+    # Fetch the actual page
+    fetch_url = 'http://mnregaweb2.nic.in/netnrega/Citizen_html/Panchregpeople.aspx'
     
-
-    cookies = {
-        'ASP.NET_SessionId': 'xpa43k45pjlzun45k4kdm245',
-    }
-
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate',
@@ -27,7 +30,7 @@ def fetch_panchayat(logger):
         'Connection': 'keep-alive',
         'DNT': '1',
         'Host': 'mnregaweb2.nic.in',
-        'Referer': 'http://mnregaweb2.nic.in/netnrega/IndexFrame.aspx?lflag=eng&District_Code=2721&district_name=AJMER&state_name=RAJASTHAN&state_Code=27&block_name=%E0%A4%85%E0%A4%B0%E0%A4%BE%E0%A4%88&block_code=2721001&fin_year=2017-2018&check=1&Panchayat_name=%E0%A4%B2%E0%A4%BE%E0%A4%AE%E0%A5%8D%E0%A4%AC%E0%A4%BE&Panchayat_Code=2721001029',
+        'Referer': url,
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:56.0) Gecko/20100101 Firefox/56.0',
     }
@@ -40,16 +43,31 @@ def fetch_panchayat(logger):
         ('Digest', '3mabBRWDbPmDl1MTHX7gjw'),
     )
 
-    logger.info(cookies)
-    response = requests.get(url, headers=headers, params=params, cookies=cookies)
-    #response = requests.get(url, headers=headers, params=params)
-    logger.info(response)
+    response = requests.get(fetch_url, headers=headers, params=params, cookies=cookies)
+    logger.debug(response.content)
+    
+    with open(filename, 'wb') as html_file:
+        logger.info('Writing [%s]' % filename)
+        html_file.write(response.content)
+
+    return 'SUCCESS'
+
+def fetch_panchregpeople(logger):
+    hard_url = 'http://mnregaweb2.nic.in/netnrega/IndexFrame.aspx?lflag=eng&District_Code=2721&district_name=AJMER&state_name=RAJASTHAN&state_Code=27&block_name=%E0%A4%85%E0%A4%B0%E0%A4%BE%E0%A4%88&block_code=2721001&fin_year=2017-2018&check=1&Panchayat_name=%E0%A4%B2%E0%A4%BE%E0%A4%AE%E0%A5%8D%E0%A4%AC%E0%A4%BE&Panchayat_Code=2721001029'
+    logger.info('Fetch URL[%s]' % hard_url)
+    response = requests.get(hard_url)
+    logger.info(response.text)
     cookies = response.cookies
     logger.info(cookies)
 
     if False:
         response = requests.get(url, headers=headers, params=params, cookies=cookies)
         logger.info(response.cookies)
+        logger.info(response.text)
+        
+        url = 'http://mnregaweb2.nic.in/netnrega/writereaddata/citizen_out/panchregpeople_2721001029_eng1718.html'
+        cmd = 'curl -L -O %s' % url
+        os.system(cmd)
     
     with open(filename, 'wb') as html_file:
         logger.info('Writing [%s]' % filename)
@@ -57,7 +75,7 @@ def fetch_panchayat(logger):
 
     return 'SUCCESS'
 
-    
+
 ##########
 # Tests
 ##########
@@ -75,9 +93,12 @@ class TestSuite(unittest.TestCase):
         cmd = 'curl -L -O %s' % url
         os.system(cmd)
 
-    def test_fetch_panchayat(self):
-        url = 'http://www.google.com/url?q=http%3A%2F%2Fmnregaweb2.nic.in%2Fnetnrega%2FIndexFrame.aspx%3Flflag%3Deng%26District_Code%3D2721%26district_name%3DAJMER%26state_name%3DRAJASTHAN%26state_Code%3D27%26block_name%3D%25E0%25A4%2585%25E0%25A4%25B0%25E0%25A4%25BE%25E0%25A4%2588%26block_code%3D2721001%26fin_year%3D2017-2018%26check%3D1%26Panchayat_name%3D%25E0%25A4%25B2%25E0%25A4%25BE%25E0%25A4%25AE%25E0%25A5%258D%25E0%25A4%25AC%25E0%25A4%25BE%26Panchayat_Code%3D2721001029&sa=D&sntz=1&usg=AFQjCNEUunLt0tnX3QGSZez-Cjs084xigg'
-        result = fetch_panchayat(self.logger)
+    def tst_fetch_panchregpeople(self):
+        result = fetch_panchregpeople(self.logger)
+        self.assertEqual('SUCCESS', result)
+
+    def test_fetch_regapp(self):
+        result = fetch_regapp(self.logger)
         self.assertEqual('SUCCESS', result)
 
 if __name__ == '__main__':
