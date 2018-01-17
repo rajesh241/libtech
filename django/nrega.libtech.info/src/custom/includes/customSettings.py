8a1d3cb11c24edac74583e921656b2d6c90d06b5
rajesh241 <togoli@gmail.com>
2018-01-17 09:46:31 +0530

    Added files in include directory

---
+++ django/nrega.libtech.info/src/custom/includes/customSettings.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+musterTimeThreshold = datetime.now() - timedelta(hours=240)
+wagelistTimeThreshold = datetime.now() - timedelta(hours=48)
+panchayatCrawlThreshold = datetime.now() - timedelta(days=7)
+panchayatRetryThreshold = datetime.now() - timedelta(hours=5)
+#jobCardRegisterTimeThreshold = datetime.now() - timedelta(days=15)
+jobCardRegisterTimeThreshold = datetime.now() - timedelta(days=4)
+telanganaJobcardTimeThreshold = datetime.now() - timedelta(days=5)
+
+repoDir="/home/libtech/repo/src/"
+djangoDir="/home/libtech/repo/django/nrega.libtech.info/src/"
+djangoSettings="libtech.settings"
+searchIP='164.100.129.6'
+startFinYear='16'
+telanganaThresholdDate="2015-04-01"
+postalWebsite="http://bdp.tsonline.gov.in/NeFMS_TS/NeFMS/Reports/NeFMS/PaymentPendingAccountsMandal.aspx"
+telanganaStateCode='36'
