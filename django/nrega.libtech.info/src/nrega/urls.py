42ee5c09e7574d35760b8f66d67c40efd6b02a7e
rajesh241 <togoli@gmail.com>
2018-01-30 08:53:22 +0530

    Adding urls.py

---
+++ django/nrega.libtech.info/src/nrega/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url
+from nrega import apiviews
+
+urlpatterns = [
+    url(r'^api/panchayats/$', apiviews.getPanchayats),
+    url(r'^api/accuratedatapts/$', apiviews.getPanchayatsAccurateData),
+    url(r'^api/states/$', apiviews.getStates),
+    url(r'^api/blocks/$', apiviews.getBlocks),
+    url(r'^api/jobcardnumber/$', apiviews.getJobcards),
+    url(r'^api/jobcardsall/$', apiviews.getJobcardsAll),    
+    url(r'^api/numberofjobcards/$', apiviews.getNumberJobcards),
+    url(r'^api/jobcard/$', apiviews.getInfoByJc),
+    url(r'^api/workdetails/$', apiviews.getWorkDetailsByJc),
+    url(r'^api/jcworkcode/$', apiviews.getTransactionByWorkCode),
+    url(r'^api/workcreditstatuspt/$', apiviews.workCreditStatusPt),
+    url(r'^api/peoplebymusterstatus/$', apiviews.peopleByMusterStatus),    
+    url(r'^api/transactionsinblock/$', apiviews.totalTransactionsForBlock),
+    url(r'^api/employmentstatus/$', apiviews.employmentStatus),        
+    url(r'^api/workdetailbypt/$', apiviews.getInfoByMusterStatus),
+    url(r'^api/musterstatus/$', apiviews.getInfoByMusterStatus_paymentDetail),
+    url(r'^api/postaldata/$', apiviews.getPostalData),
+    url(r'^api/postaldelaypt/$', apiviews.postalDelayPt),
+    url(r'^api/paymentdetails/$', apiviews.getPaymentDetail),
+    url(r'^api/daystopay/$', apiviews.paymentDays),
+    url(r'^api/crawldatapt/$', apiviews.crawlDataForPt),
+    url(r'^api/crawlstatuspt/$', apiviews.crawlStatusPt),
+]
+
