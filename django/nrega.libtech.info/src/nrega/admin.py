a47e588a5f6096c031058bd7870487de6623de4a
rajesh241 <togoli@gmail.com>
2018-02-19 05:52:57 +0530

    Made a change to admin.py

--- django/nrega.libtech.info/src/nrega/admin.py
+++ django/nrega.libtech.info/src/nrega/admin.py
@@ -169,7 +169,9 @@ class panchayatReportModelAdmin(admin.ModelAdmin):
     qs = super(panchayatReportModelAdmin, self).get_queryset(request)
     if request.user.is_superuser:
       return qs
-    return qs.filter( Q(panchayat__block__district__state__code='34') & (Q(reportType='pendingPayment') | Q(reportType='workPayment') | Q(reportType='inValidPayment') | Q(reportType="jobcardRegisterCSV") |  Q(reportType='rejectedPayment')))
+    else:
+      return qs
+#    return qs.filter( Q(panchayat__block__district__state__code='34') & (Q(reportType='pendingPayment') | Q(reportType='workPayment') | Q(reportType='inValidPayment') | Q(reportType="jobcardRegisterCSV") |  Q(reportType='rejectedPayment')))
 
 class villageReportModelAdmin(admin.ModelAdmin):
   list_display=["__str__","finyear","updateDate","get_panchayat","get_block"]
