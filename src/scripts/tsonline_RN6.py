9200ed77b29e2c48d5a0887d0bb43255edcd98b3
Mayank Rungta <mr.mynk@gmail.com>
2018-07-18 18:48:22 +0530

    Optimized the script for failures and added appropriate logs and repositioned them

--- src/scripts/tsonline_RN6.py
+++ src/scripts/tsonline_RN6.py
@@ -46,12 +46,24 @@ def fetch_rn6_report(logger, driver, state=None, district_name=None, jobcard_no=
         logger.info('File already donwnloaded. Skipping [%s]' % filename)
         return 'SUCCESS'
 
-    driver.get(url)
-    logger.info("Fetching...[%s]" % url)
+    try:
+        logger.info("Fetching...[%s]" % url)
+        driver.get(url)
+    except Exception as e:
+        logger.error('Exception when fetching url [%s] - EXCEPT[%s]' % (url, e))
+        # time.sleep(timeout)
+        driver = driverInitialize(timeout=3)  # FIXME
+        driver.get(url)
+        # return 'FAILURE'
 
-    html_source = driver.page_source.replace('<head>',
-                                         '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>')
+    try:
+        html_source = driver.page_source.replace('<head>',
+                                                 '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>')
+    except Exception as e:
+        logger.error('Exception getting HTML source - EXCEPT[%s]' % e)
+        return 'FAILURE'
     logger.debug("HTML Fetched [%s]" % html_source)
+    
 
     bs = BeautifulSoup(html_source, 'html.parser')
 
@@ -66,7 +78,8 @@ def fetch_rn6_report(logger, driver, state=None, district_name=None, jobcard_no=
         # elem.click()
         # elem = driver.find_element_by_xpath("//select[@name='ctl00$MainContent$ddlDistrict']/option[text()=district_name]").click()
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s]' % e)
+        logger.error('Exception during Select() - EXCEPT[%s]' % e)
+        # return 'FAILURE'
     # time.sleep(timeout)
 
     try:
@@ -74,7 +87,7 @@ def fetch_rn6_report(logger, driver, state=None, district_name=None, jobcard_no=
         select.select_by_visible_text('MGNREGA')
         logger.info('MGNREGA selected')
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s]' % e)
+        logger.error('Exception during Select() - EXCEPT[%s]' % e)
 
     try:
         # elem = driver.find_element_by_id('ctl00_MainContent_txtSSPPEN')
@@ -82,7 +95,8 @@ def fetch_rn6_report(logger, driver, state=None, district_name=None, jobcard_no=
         elem.click()
         logger.info('Clicked Jobcard radio button')
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s]' % e)
+        logger.error('Exception Jobcard radio button - EXCEPT[%s]' % e)
+        # return 'FAILURE'
 
     try:
         elem = driver.find_element_by_id('ctl00_MainContent_txtSSPPEN')
@@ -90,34 +104,35 @@ def fetch_rn6_report(logger, driver, state=None, district_name=None, jobcard_no=
         elem.click()
         logger.info('Entering Jobcard[%s]' % jobcard_no)
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s]' % e)
+        logger.error('Exception Entering Jobcard[%s] - EXCEPT[%s]' % (jobcard_no,e))
+        # return 'FAILURE'
     #time.sleep(timeout)
 
     try:
+        logger.info('Clicking Submit')
         elem = driver.find_element_by_id('ctl00_MainContent_btnMakePayment')
         elem.click()
-        logger.info('Clicking Submit')
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s] for jobcard[%s]' % (e, jobcard_no))
+        logger.error('Exception Clicking Submit for jobcard[%s] - EXCEPT[%s]' % (jobcard_no,e))
         return 'FAILURE'
     #time.sleep(timeout)
 
 
     try:
+        logger.info('Clicking Select radio button')
         elem = driver.find_element_by_css_selector("input[type='radio'][value='rbSelect']")
         elem.click()
-        logger.info('Clicked Select radio button')
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s] for jobcard[%s]' % (e, jobcard_no))
+        logger.error('Exception selecting radio button for jobcard[%s] - EXCEPT[%s]' % (jobcard_no,e))
         return 'FAILURE'
     #time.sleep(timeout)
     
     try:
+        logger.info('Clicking View Ledger')
         elem = driver.find_element_by_id('ctl00_MainContent_btnTxDetails')
         elem.click()
-        logger.info('Clicked View Ledger')
     except Exception as e:
-        logger.error('Ouch! Caught Exception[%s]' % e)
+        logger.error('Exception clicking view ledger for jobcard[%s] - EXCEPT[%s]' % (jobcard_no,e))
         return 'FAILURE'
     #time.sleep(2)
 
@@ -133,12 +148,13 @@ def fetch_rn6_report(logger, driver, state=None, district_name=None, jobcard_no=
         return 'FAILURE'
 
     try:
+        logger.info('Waiting for the dialog box to open')
         elem = WebDriverWait(driver, timeout).until(
           EC.presence_of_element_located((By.CLASS_NAME, "btn"))
         )
-    except NoSuchElementException or TimeoutException:
-        logger.error("Failed to locate Close Button")
-        driver.save_screenshot('./logs/button_'+jcno+'.png')
+    except Exception as e:
+        logger.error('Exception on WebDriverWait(10) - EXCEPT[%s]' % e)
+        driver.save_screenshot('./button_'+jobcard_no+'.png')
         driver.close()
         driver.switch_to.window(parent_handle)
         return 'FAILURE'
@@ -160,8 +176,8 @@ def fetch_rn6_reports(logger, driver):
     logger.info('Fetch the jobcards')
 
     if False:
-        result = fetch_rn6_report(logger, driver, state='ap', district_name='ANANTAPUR', jobcard_no='121673411011010257-02')
-        result = fetch_rn6_report(logger, driver, district_name='MAHABUBNAGAR', jobcard_no='142000520007010015-03')
+        # result = fetch_rn6_report(logger, driver, state='ap', district_name='ANANTAPUR', jobcard_no='121673411011010257-02')
+        result = fetch_rn6_report(logger, driver, district_name='MAHABUBNAGAR', jobcard_no='141990515024010071-08')
         return 'SUCCESS'
 
     district_name = 'MAHABUBNAGAR'
@@ -178,7 +194,7 @@ def fetch_rn6_reports(logger, driver):
             raise        
     
     try:
-        logger.info('Fetching URL[%s]' % url)
+        logger.info('Requesting URL[%s]' % url)
         response = requests.get(url, timeout=timeout) # , cookies=cookies)
     except Exception as e:
         logger.error('Caught Exception[%s]' % e) 
@@ -187,14 +203,19 @@ def fetch_rn6_reports(logger, driver):
     logger.debug('Panchayats JSON[%s]' % panchayats_json)
 
     
+    is_panchayat = True
     for panchayat_object in panchayats_json:
         panchayat_name = panchayat_object['name'].strip()
         panchayat_code = panchayat_object['code'].strip()
         logger.info('Fetch jobcards for Panchayat[%s, %s]' % (panchayat_name, panchayat_code))
         
+        if is_panchayat and panchayat_name != 'VITHALAPUR':
+            logger.debug('Already downloaded. Skipping[%s]' % panchayat_name)
+            continue
+        is_panchayat = False
         url = 'http://b.libtech.info:8000/api/getworkers/?pcode=%s' % panchayat_code
         try:
-            logger.info('Fetching URL[%s]' % url)
+            logger.info('Requesting URL[%s]' % url)
             response = requests.get(url, timeout=timeout)
         except Exception as e:
             logger.error('Caught Exception[%s]' % e)
@@ -203,13 +224,17 @@ def fetch_rn6_reports(logger, driver):
         flag = True
         for jobcard_object in jobcards_json:
             jobcard = '%s-0%s' % (jobcard_object['jobcard']['tjobcard'], jobcard_object['applicantNo'])
-            if panchayat_name == 'ANNASAGAR' and flag and (jobcard != '142000520007010299-01'):
-                logger.info('Skipping[%s]' % jobcard)
+            if (panchayat_name == 'VITHALAPUR' and flag and (jobcard != '142000501002010385-01')): 
+                logger.debug('Skipping[%s]' % jobcard)
                 continue            
             flag = False
             logger.info('Fetch details for jobcard[%s]' % jobcard)
-            fetch_rn6_report(logger, driver, district_name=district_name, jobcard_no=jobcard, block_name=block_name, panchayat_name=panchayat_name)
-    
+            result = fetch_rn6_report(logger, driver, district_name=district_name, jobcard_no=jobcard, block_name=block_name, panchayat_name=panchayat_name)
+            if result != 'SUCCESS':
+                logger.error('Failure returned [%s]' % result)
+                #time.sleep(3)
+                continue  # FIXME why is this even needed. Why is it not working?
+
     return 'SUCCESS'
 
 class TestSuite(unittest.TestCase):
@@ -217,10 +242,10 @@ class TestSuite(unittest.TestCase):
         self.logger = loggerFetch('info')
         self.logger.info('BEGIN PROCESSING...')
         self.display = displayInitialize(0)
-        self.driver = driverInitialize(path='/opt/firefox/', timeout=3)
+        self.driver = driverInitialize(timeout=3, options='--headless') # driverInitialize(path='/opt/firefox/', timeout=3)
 
     def tearDown(self):
-        #FIXME driverFinalize(self.driver) 
+        #driverFinalize(self.driver) 
         displayFinalize(self.display)
         self.logger.info('...END PROCESSING')
 
