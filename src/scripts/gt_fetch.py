7252f084301df18296d22982606b4a41f38a986e
mynk <mr.mynk@gmail.com>
2020-02-15 18:58:01 +0530

    Completed the GT script for a single household with Requests and Selenium

--- src/scripts/gt_fetch.py
+++ src/scripts/gt_fetch.py
@@ -53,7 +53,7 @@ base_url = 'https://meebhoomi.ap.gov.in/'
 village_list = [('విశాఖపట్నం', 'అచ్యుతాపురం', 'జోగన్నపాలెం'), ('విశాఖపట్నం', 'అనంతగిరి', 'నిన్నిమామిడి'), ('విశాఖపట్నం', 'అనందపురం', 'ముచ్చెర్ల')]
 skip_district = ['3',]
 is_visible = True
-is_mynk = True
+is_virtual = True
 
 
 #############
@@ -68,23 +68,29 @@ is_mynk = True
 #############
 
 class GeorgeTown():
-    def __init__(self):
+    def __init__(self, is_selenium=None):
         self.fs_url = 'https://nfs.delhi.gov.in/Citizen/Householdtobeincluded.aspx'
         self.status_file = 'status.csv'
-        self.dir = 'data/'
+        self.dir = 'data'
         try:
             os.makedirs(self.dir)
         except OSError as e:
             if e.errno != errno.EEXIST:
-                raise    
-        
-        self.display = displayInitialize(isDisabled = True, isVisible = is_visible)
-        self.driver = driverInitialize(timeout=3)
-        #self.driver = driverInitialize(path='/opt/firefox/', timeout=3)
+                raise
+
+        self.is_selenium = False
+        if is_selenium:
+            self.is_selenium = is_selenium
+
+        if self.is_selenium:
+            self.display = displayInitialize(isDisabled = not is_virtual, isVisible = is_visible)
+            self.driver = driverInitialize(timeout=3)
+            #self.driver = driverInitialize(path='/opt/firefox/', timeout=3)
 
     def __del__(self):
-        driverFinalize(self.driver) 
-        displayFinalize(self.display)
+        if self.is_selenium:
+            driverFinalize(self.driver) 
+            displayFinalize(self.display)
       
     def wait_for_window(self, timeout = 2):
         time.sleep(round(timeout / 1000))
@@ -177,8 +183,22 @@ class GeorgeTown():
 
 
     def request_fs_report(self, logger, circle=None, fps=None):
+        url = 'https://nfs.delhi.gov.in/Citizen/Householdtobeincluded.aspx'
+        logger.info(f'Fetching URL[{url}]')
+        response = requests.get(url, verify=False)
+        filename=f'{self.dir}/{circle}_{fps}.html'
+        with open(filename, 'wb') as html_file:
+            logger.info(f'Writing file[{filename}]')
+            html_file.write(response.content)
+
+        bs = BeautifulSoup(response.content, 'html.parser')
+        view_state = bs.find('input', attrs={'id':'__VIEWSTATE'})['value']
+        event_validation = bs.find('input', attrs={'id':'__EVENTVALIDATION'})['value']
+        session_id = response.cookies['ASP.NET_SessionId']
+        logger.info(f'Cookies[ASP.NET_SessionId:{session_id}] and vs[{view_state}] and ev[{event_validation}]')
+
         cookies = {
-            'ASP.NET_SessionId': 'xjw2ze5bneieggjx3010atic',
+            'ASP.NET_SessionId': session_id,
         }
         
         headers = {
@@ -192,20 +212,19 @@ class GeorgeTown():
             'Referer': 'https://nfs.delhi.gov.in/Citizen/Householdtobeincluded.aspx',
             'Upgrade-Insecure-Requests': '1',
         }
-        
+
         data = {
-          '__EVENTTARGET': '',
-          '__EVENTARGUMENT': '',
-          '__LASTFOCUS': '',
-          '__VIEWSTATE': '/wEPDwUKLTE5NTA4OTE2OA9kFgJmD2QWAgIDD2QWBAITDw8WAh4HVmlzaWJsZWhkZAIVD2QWBgIJDxAPFgYeDkRhdGFWYWx1ZUZpZWxkBQhDaXJjbGVObx4NRGF0YVRleHRGaWVsZAUKQ2lyY2xlTmFtZR4LXyFEYXRhQm91bmRnZBAVRwwtLS1TZWxlY3QtLS0RQURBUlNIIE5BR0FSIFswNF0TQU1CRURLQVIgTkFHQVIgWzQ4XQ1CQUJBUlBVUiBbNjddDUJBREFSUFVSIFs1M10KQkFETEkgWzA1XQ9CQUxMSU1BUkFOIFsyMl0LQkFXQU5BIFswN10NQklKV0FTQU4gWzM2XQtCVVJBUkkgWzAyXRJDSEFORE5JIENIT1dLIFsyMF0QQ0hIQVRUQVJQVVIgWzQ2XRBERUxISSBDQU5UVCBbMzhdCkRFT0xJIFs0N10LRFdBUktBIFszM10RR0FOREhJIE5BR0FSIFs2MV0LR0hPTkRBIFs2Nl0NR09LQUxQVVIgWzY4XRRHUkVBVEVSIEtBSUxBU0ggWzUwXQ9IQVJJIE5BR0FSIFsyOF0OSkFOQUtQVVJJIFszMF0NSkFOR1BVUkEgWzQxXQxLQUxLQUpJIFs1MV0SS0FSQVdBTCBOQUdBUiBbNzBdD0tBUk9MIEJBR0ggWzIzXRNLQVNUVVJCQSBOQUdBUiBbNDJdC0tJUkFSSSBbMDldC0tPTkRMSSBbNTZdEktSSVNITkEgTkFHQVIgWzYwXRBMQVhNSSBOQUdBUiBbNThdDE1BRElQVVIgWzI2XRJNQUxWSVlBIE5BR0FSIFs0M10PTUFOR09MUFVSSSBbMTJdDE1BVElBTEEgWzM0XRBNQVRJQSBNQUhBTCBbMjFdDU1FSFJBVUxJIFs0NV0PTU9ERUwgVE9XTiBbMThdD01PVEkgTkFHQVIgWzI1XQtNVU5ES0EgWzA4XQ9NVVNUQUZBQkFEIFs2OV0OTkFKQUZHQVJIIFszNV0QTkFOR0xPSSBKQVQgWzExXQtOQVJFTEEgWzAxXQ5ORVcgREVMSEkgWzQwXQpPS0hMQSBbNTRdClBBTEFNIFszN10QUEFURUwgTkFHQVIgWzI0XQ9QQVRQQVJHQU5KIFs1N10TUkFKRU5ERVIgTkFHQVIgWzM5XRJSQUpPUkkgR0FSREFOIFsyN10MUklUSEFMQSBbMDZdDlIuSyBQVVJBTSBbNDRdC1JPSElOSSBbMTNdElJPSFRBU0ggTkFHQVIgWzY0XRBTQURBUiBCQVpBUiBbMTldEVNBTkdBTSBWSUhBUiBbNDldDlNFRUxBTVBVUiBbNjVdDlNFRU1BUFVSSSBbNjNdDVNIQUhEQVJBIFs2Ml0RU0hBS1VSIEJBU1RJIFsxNV0SU0hBTElNQVIgQkFHSCBbMTRdFFNVTFRBTlBVUiBNQUpSQSBbMTBdEFRJTEFLIE5BR0FSIFsyOV0NVElNQVJQVVIgWzAzXRBUUklMT0sgUFVSSSBbNTVdDlRSSSBOQUdBUiBbMTZdEFRVR0hMQUtBQkFEIFs1Ml0QVVRUQU0gTkFHQVIgWzMyXQ9WSUtBUyBQVVJJIFszMV0SVklTSFdBUyBOQUdBUiBbNTldDVdBWklSUFVSIFsxN10VRwEwCDAxMDYwMDA0CDAxMDIwMDQ4CDAxMDkwMDY3CDAxMDIwMDUzCDAxMDgwMDA1CDAxMDUwMDIyCDAxMDgwMDA3CDAxMDMwMDM2CDAxMDYwMDAyCDAxMDUwMDIwCDAxMDIwMDQ2CDAxMDMwMDM4CDAxMDIwMDQ3CDAxMDMwMDMzCDAxMDQwMDYxCDAxMDkwMDY2CDAxMDkwMDY4CDAxMDEwMDUwCDAxMDcwMDI4CDAxMDcwMDMwCDAxMDEwMDQxCDAxMDIwMDUxCDAxMDkwMDcwCDAxMDUwMDIzCDAxMDEwMDQyCDAxMDgwMDA5CDAxMDQwMDU2CDAxMDQwMDYwCDAxMDQwMDU4CDAxMDcwMDI2CDAxMDEwMDQzCDAxMDcwMDEyCDAxMDMwMDM0CDAxMDUwMDIxCDAxMDIwMDQ1CDAxMDYwMDE4CDAxMDUwMDI1CDAxMDgwMDA4CDAxMDkwMDY5CDAxMDMwMDM1CDAxMDcwMDExCDAxMDgwMDAxCDAxMDEwMDQwCDAxMDEwMDU0CDAxMDMwMDM3CDAxMDUwMDI0CDAxMDQwMDU3CDAxMDMwMDM5CDAxMDcwMDI3CDAxMDgwMDA2CDAxMDEwMDQ0CDAxMDgwMDEzCDAxMDkwMDY0CDAxMDUwMDE5CDAxMDIwMDQ5CDAxMDkwMDY1CDAxMDkwMDYzCDAxMDQwMDYyCDAxMDYwMDE1CDAxMDYwMDE0CDAxMDgwMDEwCDAxMDcwMDI5CDAxMDYwMDAzCDAxMDQwMDU1CDAxMDYwMDE2CDAxMDIwMDUyCDAxMDMwMDMyCDAxMDcwMDMxCDAxMDQwMDU5CDAxMDYwMDE3FCsDR2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnFgECAmQCDA8QDxYGHwEFB2Zwc2NvZGUfAgUHRnBzVGV4dB8DZ2QQFRkMLS0tU2VsZWN0LS0tKDEwMDMwMDQwMDAwMVszMDEzXSBNL1MgTkFSQVlBTiBMQUwgR09ZQUwpMTAwMzAwNDAwMDAzWzM2NzldIE0vcyBST09ETUFMIFJBTSBLSVNIQU4hMTAwMzAwNDAwMDA0WzM3NDldIE0vUyBKSEFOR0kgUkFNKjEwMDMwMDQwMDAwNVs0NzA4XSBNL1MgU0hZQU0gU1VOREVSIEdBVVlBTCAxMDAzMDA0MDAwMDZbNDk5MV0gTS9TIFJBTSBTQUhBWSExMDAzMDA0MDAwMDhbNTAyNF0gTS9TIEtBTUxBIERFVkkmMTAwMzAwNDAwMDEwWzUwNjZdIE0vUyBSQUpBU1RIQU4gU1RPUkUiMTAwMzAwNDAwMDExWzUyMzddIE0vUyBOQU5BSyBDSEFORCExMDAzMDA0MDAwMTJbNjA4OF0gTS9TIEtIRU0gQ0hBTkQsMTAwMzAwNDAwMDEzWzYwODldIE0vUyBIQVJJIENIQVJBTiBSQU0gQVZUQVIfMTAwMzAwNDAwMDE1WzYxMTFdIE0vUyBIQU5TIFJBSiAxMDAzMDA0MDAwMTZbNjIzMV0gTS9TIFNBR0FSIE1BTCIxMDAzMDA0MDAwMTdbNjU0Ml0gTS9TIFNIQU5LQVIgTEFMITEwMDMwMDQwMDAxOFs2NTUyXSBNL1MgSE9MSSBTVE9SRSAxMDAzMDA0MDAwMTlbNjU1M10gTS9TIE1PSEFOIExBTCAxMDAzMDA0MDAwMjBbNjU1Nl0gTS9TIFJBTSBQWUFSSSExMDAzMDA0MDAwMjRbNjk4N10gTS9TIFNISVYgS1VNQVIlMTAwMzAwNDAwMDI1WzcwMDZdIE0vUyBKQUdEQU1CQSBTVE9SRSIxMDAzMDA0MDAwMjZbNzQ4NV0gTS9TIEdPWUFMIFNUT1JFITEwMDMwMDQwMDAyOVs3NTM1XSBNL1MgU0hJViBTVE9SRS4xMDAzMDA0MDAwMzJbNzgwNl0gTS9TIEdBUkcgRElQQVJUTUVOVEFMIFNUT1JFJDEwMDMwMDQwMDAzM1s3ODEyXSBNL1MgQkFKUkFORyBTVE9SRSkxMDAzMDA0MDAwMzRbNzg0OV0gTS9TIE1BSEFMQUtTSEFNSSBTVE9SRSIxMDAzMDA0MDAwMzVbODUxMV0gTS9TIExBWE1JIFNUT1JFFRkBMAwxMDAzMDA0MDAwMDEMMTAwMzAwNDAwMDAzDDEwMDMwMDQwMDAwNAwxMDAzMDA0MDAwMDUMMTAwMzAwNDAwMDA2DDEwMDMwMDQwMDAwOAwxMDAzMDA0MDAwMTAMMTAwMzAwNDAwMDExDDEwMDMwMDQwMDAxMgwxMDAzMDA0MDAwMTMMMTAwMzAwNDAwMDE1DDEwMDMwMDQwMDAxNgwxMDAzMDA0MDAwMTcMMTAwMzAwNDAwMDE4DDEwMDMwMDQwMDAxOQwxMDAzMDA0MDAwMjAMMTAwMzAwNDAwMDI0DDEwMDMwMDQwMDAyNQwxMDAzMDA0MDAwMjYMMTAwMzAwNDAwMDI5DDEwMDMwMDQwMDAzMgwxMDAzMDA0MDAwMzMMMTAwMzAwNDAwMDM0DDEwMDMwMDQwMDAzNRQrAxlnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZGQCDw88KwARAgEQFgAWABYADBQrAABkGAEFG2N0bDAwJE1haW5Db250ZW50JEdyaWRWaWV3MQ9nZMSnegm4rCGcOkYoVepSNF+2YM9ZeOl0CSIWgKulR2h6',
-          '__EVENTVALIDATION': '/wEdAGRz3SXbnGxLgD3U4Dpd593+Z4qP+sY8CZwvfWpHtu6CVr1kYiIG0/Zw3uOH4kCIoCGpY8WWEAXL/ZO4PD1YJUMwKTECnh1R7DlS9r2JyOBRy0yGw44Mhw2jeCPOd9R3/C4Xv3RiJmjBDzU8nPtLSx4shojg4tkP08xY9B/EbZ3dTRf3PM9brUAAxDt58Hp9rhQSzbJEQA5gg8vm7HzcjK+uKpU7jMyBaNyetqINz89GLcg7VeIlldQ3tTNayx6zwzrl7A/cNwOIqIygab27KQb7HeHt9WSXBPhBjR9fr7WU1N6CK07cMqIp0wG7BauERt5vwqfh0JGyXRGYw+O68YLnmamR0gUdMdP9Z/hk8BOJyhC6Lhmg2d9wPkaCU4PR0dgQE6H31rAO8Y/ceMk7nDHKT2CbnXXgGaLptZDZYQ9N6OKrGlSXi05RL5sRBsonCnr9QtHQknYxYUzH49cFjbvM+9NUgH3Wq+PHwNfTwRKDF7u6YRnz7UF0TvjI63Hu79ZqhJd1muZbtV94lL1oxcv+UBCZIo97Tu8NcH7uqORKsRfTB4GQaOSKJgtXM68pH2QNe4cAsuh1+qVYInncfPXltIFCZFX9lRN0jvGo2GRaSQCPcOqcW7aRNar31/pKjUOqiCzd2mT49SH2O/shWWgNAKVYIftrRS5WXqZnyeRqV1VIJFh2/NaliyRQ07yN6nEiaPiDHxt6aWCoyOexndynVEO9jZq93hrKE2Zr3/6aj8uyZgjeIjqBVqgvpRGWmCI2DIrr2InyM2gjoNiOAeWNWStJ0c0tw1T4RMTcs7PUxmiUbmMzsBxYHLoI571XQH7UaFAbWXx+SKGJeb2f1rLNHC1/V6KPTvAmVvSz8BDLVxDhIzvnBkkqtb011t0JYMFKVXkgaf4v1nbIZJ9OMenkqnixX7LVKczIJpxQku/QbkJB8JTpbxDN5ctnaLJydu+QUg6KMZpS9L6EeEu/gLE74Iz2QGQvsg60UJpCKiLcaMVaZzGegGzny+eO3dqHNfWilZ2Pot0D/fYoFx0ZUpUtJ/ZEbKuxiw1YaNWHLv5KaprZIkgKw0VfJtiyszlemAe8DANxTbxxj6lcXPP/20ABSSQkXJ5Otw9Kf6EK8gcPt4/k9+EriXH0rLTIZG29L4TEp5+usIJXxj4AroDzXWjvfei5h/cTcpuEMh/3muPGHK7AbtgKv8nPzdDXGRZzD21Otk+mSZ4huD/EJgzDa7mjrmG3IWBEx2lO9aHG5fiIo9N/zzm+1AEw0PvpRxhnyly+gQnLceAb6iyLX/tvydGJ3meXho1TPqKzlzUPonMN6K+GxEt3CexRS8UGY/u50hpeLw0RT9xUUAz2ivlQvJZJg8ap8LvyE7sbB5NkUvNpCdk1KTcfuSibt9Q7JJO3vNWte7Hhrji1KXWvvrdd++Tjni6JWI/7HD0Ss8nHsemBHoUnjUsoL9E4uAIXQlFvTj33xAK4YFLdYXjR0jb44gBjSGPI1unQ0OST7XYryv4ccjXd/6Ij7M+sjM1vhchUoS3GmMhZYuE/xfLQkTvHbJ+Dt3hES8gC9b6GuSP0hWmgtClZvHuPco7PDv2DkOJ9FBrMb7Lqj2OhlUxsMmxowJveIQDC14omPnikWNknXg2mIu6W2CnmclosHw+9rpA6itD5nIhRsk86Ap5b7GIyTOvlUBSNJcuXtSfbC20Ija8JGre9sLmIKxVJ5bB+QC7WX/fGGzpEw33mR+62HGoI/tMqlJRvUn6b5ldtb+6qkiuQ3SbKQhBCuWJAG2cr1ogn9mTC3/bP7DBCsSPGD3m2n7q+AHFhjCs3QHPyz9dbyqkf9ITi2AhXGXpQXsPbYtAJfMyol2Txpp28VZ45zWYINld04nP2M/xUVaHHvyQwcyEt+hY8cyyEPRmaJJ9DgY+KSKYDjYTtC6hwiz9MW33pXKeaIa8M1MMlIONKY7nvIPqTNtt6ssdjJpLdFqtuTMirxaEsiLWevLGrW/75DHNJfvNl0rgkBd2cSh1uiL27QlsA1Z6BjL97tizb0zyaM1+9celPdZMdjOk4gbt3AXkZaZ1oHUQeJkCQNI0hW/iLQBeDkxuRfL6/FPME27z4fRlf155CeHmQnidI93i04727bqrnHjd1edN//8Eqo1iS4nKzwHjEN18xqIclPiaHFRhpoRQ=',
-          'ctl00$MainContent$ddlCircle': circle,
-          'ctl00$MainContent$ddlFps': fps,
-          'ctl00$MainContent$btnview': 'Show'
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__LASTFOCUS': '',
+            '__VIEWSTATE': view_state, 
+            '__EVENTVALIDATION': event_validation,
+            'ctl00$MainContent$ddlCircle': circle,
+            'ctl00$MainContent$ddlFps': fps,
+            'ctl00$MainContent$btnview': 'Show'
         }
-        
-        response = requests.post('https://nfs.delhi.gov.in/Citizen/Householdtobeincluded.aspx', headers=headers, cookies=cookies, data=data, verify=False)
-        filename=f'{self.dir}/{circle}_{fps}.csv'
+
+        response = requests.post(url, headers=headers, cookies=cookies, data=data, verify=False)
         with open(filename, 'wb') as html_file:
             logger.info(f'Writing file[{filename}]')
             html_file.write(response.content)
@@ -216,23 +235,73 @@ class GeorgeTown():
         logger.info(f'Fetching URL[{url}]')
         driver.get(url)
         try:
-            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_ddlCircle"]')))
+            WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_btnview"]')))
             logger.info(f'Fetching Food Security report for Circle[{circle}] and FPS[{fps}]...')
         except TimeoutException:
             logger.error(f'Timed out waiting for drop down for Circle[{circle}] and FPS[{fps}]')
-            #break
-            #return 'FAILURE'
+            # return 'FAILURE'
         except Exception as e:
             logger.error(f'Errored Waiting for Food Security report, Circle[{circle}] and FPS[{fps}] - EXCEPT[{type(e)}, {e}]')
-            #break
-            #return 'FAILURE'
+            # return 'FAILURE'
+
+        # elem = driver.find_element_by_xpath('//*[@id="MainContent_ddlCircle"]')
+        select = Select(driver.find_element_by_id('MainContent_ddlCircle'))
+        select.select_by_value(circle)
+
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, f'#MainContent_ddlFps > option[value="{fps}"]'))
+            )
+        except TimeoutException:
+            logger.error(f'Timed out waiting for drop down for FPS[{fps}]')
+            # return 'FAILURE'
+        except Exception as e:
+            logger.error(f'Errored Waiting for Food Security report, for FPS[{fps}] - EXCEPT[{type(e)}, {e}]')
+            # return 'FAILURE'
+
+        select = Select(driver.find_element_by_id('MainContent_ddlFps'))
+        select.select_by_value(fps)
+
+        time.sleep(3)
+
+        '''
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, f''))  # Ideal would be to check if Circle would be available
+            )
+        except TimeoutException:
+            logger.error(f'Timed out waiting for drop down for FPS[{fps}]')
+            # return 'FAILURE'
+        except Exception as e:
+            logger.error(f'Errored Waiting for Food Security report, for FPS[{fps}] - EXCEPT[{type(e)}, {e}]')
+            # return 'FAILURE'
+        '''
+        
+        try:
+            show_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_btnview"]')))
+            logger.info(f'Fetching Food Security report for Circle[{circle}] and FPS[{fps}]...')
+        except TimeoutException:
+            logger.error(f'Timed out waiting for drop down for Circle[{circle}] and FPS[{fps}]')
+            # return 'FAILURE'
+        except Exception as e:
+            logger.error(f'Errored Waiting for Food Security report, Circle[{circle}] and FPS[{fps}] - EXCEPT[{type(e)}, {e}]')
+            # return 'FAILURE'
+
+        show_button.click()
+
+        html_source = self.driver.page_source
+        filename=f'{self.dir}/{circle}_{fps}.html'
+        with open(filename, 'w') as html_file:
+            logger.info(f'Writing file[{filename}]')
+            html_file.write(html_source)
 
         filename=f'{self.dir}/{circle}_{fps}.csv'
-        html_source = self.driver.page_source
         df = pd.read_html(html_source)[0]
         logger.debug(f'{df}')
         logger.info(f'Writing [{filename}]') 
         df.to_csv(filename, index=False)
+        
+        return 'SUCCESS'   # Leaving it at this
 
         villages = df['Village Name']
         
@@ -342,7 +411,7 @@ class TestSuite(unittest.TestCase):
     def test_crawl_fs_report(self):
         self.logger.info("Running test for Food Security Report")
         # Start a RhythuBharosa Crawl
-        gt = GeorgeTown()
+        gt = GeorgeTown(is_selenium=True)
         gt.crawl_fs_report(self.logger, circle='01020048', fps='100300400015')
         del gt
         
@@ -350,7 +419,7 @@ class TestSuite(unittest.TestCase):
         self.logger.info("Running test for Food Security Report")
         # Start a RhythuBharosa Crawl
         gt = GeorgeTown()
-        #gt.request_fs_report(self.logger, circle='01020048', fps='100300400015')
+        gt.request_fs_report(self.logger, circle='01020048', fps='100300400015')
         gt.request_fs_report(self.logger, circle='01090067', fps='100500500005')
         del gt
         
